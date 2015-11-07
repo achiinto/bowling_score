40fa084b6c1b38618a18ff951f6bbd3d85376f8b
Ian Choi <achiinto@gmail.com>
2015-11-07 23:54:39 +0800

    Feature #1 - Score Calculator intialization

--- lib/bowling_score/score_calculator.rb
+++ lib/bowling_score/score_calculator.rb
@@ -2,5 +2,9 @@ module BowlingScore
   # Score Calculator - taking an array or string of scores (integers)
   #                    and calculate with strike and spare rewarding
   class ScoreCalculator
+    def initialize(scores)
+      scores = scores.split(',') if scores.is_a? String
+      @scores = scores.map(&:to_i)
+    end
   end
 end
